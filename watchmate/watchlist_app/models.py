3f6e2c980adb747e23a8faeb45c4451491752361
Emydev-op <alphaoseghe@gmail.com>
2023-08-24 23:03:46 +0100

    Made some changes

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -29,5 +29,4 @@ class Review(models.Model):
     updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.rating
-    
+        return str(self.rating) + ' | ' + self.watchlist.title
