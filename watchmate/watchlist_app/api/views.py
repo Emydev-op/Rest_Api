f692ffd3817a3c2a1c336ef3e524f959d789ff1d
Emydev-op <alphaoseghe@gmail.com>
2023-08-26 18:52:41 +0100

    created a generic view with mixin and updated the views and urls for the reviews

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -17,6 +17,13 @@ class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Generi
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+    
+class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 class WatchListAView(APIView):
     def get(self, request):
